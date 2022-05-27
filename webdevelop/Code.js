45dc56773df3f51b1e349658163030aa065e3644
nishino <a-nishino@esm.co.jp>
2022-05-27 17:22:08 +0900

    Webアプリ開発（2-1,2-2）

--- webdevelop/Code.js
+++ webdevelop/Code.js
@@ -1,3 +1,23 @@
 function doGet() {
   return HtmlService.createTemplateFromFile('index').evaluate();
 }
+
+// スプレッドシートに値を設定する
+function setSheetData(value) {
+  const SHEET_ID = '16EgRD0EgamTEq21ojp9k2z5VXJNsuOmXuNH-Jt9sfq0'
+  const SHEET_NAME = 'シート1'
+  const spreadsheet = SpreadsheetApp.openById(SHEET_ID);
+  const sheet = spreadsheet.getSheetByName(SHEET_NAME);
+  const range = sheet.getRange('A1');
+  range.setValue(value);
+}
+
+// スプレッドシートから値を取得する
+function getSheetData() {
+  const SHEET_ID = '16EgRD0EgamTEq21ojp9k2z5VXJNsuOmXuNH-Jt9sfq0'
+  const SHEET_NAME = 'シート1'
+  const spreadsheet = SpreadsheetApp.openById(SHEET_ID);
+  const sheet = spreadsheet.getSheetByName(SHEET_NAME);
+  const range = sheet.getRange('A1');
+  return range.getValue();
+}
