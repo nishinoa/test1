2b2abe3a0ebd9ccea3923994c9ba3342ffb5f0e3
nishino <a-nishino@esm.co.jp>
2022-08-20 23:21:19 +0900

    Webアプリ開発2（3-2-1～3-2-3）

--- web_application_2/src/view/BookApp.vue
+++ web_application_2/src/view/BookApp.vue
@@ -1,16 +1,205 @@
 <template>
-  <div>
-    書籍管理アプリだよ
+  <div class="list">
+    <v-card-title>
+      <v-text-field v-model="searchTitle" append-icon="mdi-magnify" label="タイトルの文字列検索" />
+      <v-select
+        v-model="searchCategory"
+        :items="bookList"
+        item-text="category"
+        label="ジャンル検索"
+        ></v-select>
+      <v-btn @click="onClickSearchBtn">検索</v-btn>
+      <v-btn @click="onClickAddBtn">新規登録</v-btn>
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="bookList"
+      class="elevation-1"
+    >
+      <template v-slot:top>
+          <v-dialog v-model="isShowDialog" max-width="500px">
+            <v-card>
+              <v-card-title>
+                <span class="headline">登録／編集</span>
+              </v-card-title>
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                    <v-col cols="6">
+                      <v-text-field v-model="dialogBookList.title" label="タイトル" />
+                    </v-col>
+                    <v-col cols="6">
+                      <v-text-field v-model="dialogBookList.category" label="ジャンル" />
+                    </v-col>
+                    <v-col cols="6">
+                      <v-menu
+                        ref="menu"
+                        v-model="menu"
+                        :close-on-content-click="false"
+                        :return-value.sync="pickerDate"
+                        transition="scale-transition"
+                        offset-y
+                        min-width="auto"
+                      >
+                        <template #activator="{ on, attrs }" >
+                          <v-text-field
+                            v-model="textDate"
+                            label="購入日"
+                            prepend-icon="mdi-calendar"
+                            readonly
+                            v-bind="attrs"
+                            v-on="on" />
+                        </template>
+                          <v-date-picker
+                            v-model="pickerDate"
+                            no-title
+                            scrollable
+                            @input="formatDate(pickerDate)
+                            $refs.menu.save(pickerDate)
+                            menu = false
+                            "
+                          >
+                          </v-date-picker>
+                      </v-menu>
+                    </v-col>
+                    <v-col cols="6">
+                      <v-text-field v-model="dialogBookList.buyer" label="購入者" />
+                    </v-col>
+                    <v-col cols="12">
+                      <v-text-field v-model="dialogBookList.review_content" label="レビュー内容" />
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn @click="closeDialog">閉じる</v-btn>
+                <v-btn @click="onClickInsertUpdateBtn">保存</v-btn>
+                <v-spacer />
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+          <v-dialog v-model="isShowDeleteDialog" max-width="500px">
+          <v-card>
+            <v-card-title class="text-h5">削除しますか?</v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn @click="closeDeleteDialog">キャンセル</v-btn>
+              <v-btn @click="onClickDeleteBtn">削除</v-btn>
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </template>
+      <template>
+        <td>{{ bookList.title }}</td>
+        <td>{{ bookList.category }}</td>
+        <td>{{ bookList.purchase_date }}</td>
+        <td>{{ bookList.buyer }}</td>
+      </template>
+      <template v-slot:[`item.edit-action`]="{ item }">
+        <v-icon @click="onClickEditIcon(item)">mdi-pencil</v-icon>
+      </template>
+      <template v-slot:[`item.delete-action`]="{ item }">
+        <v-icon @click="onClickDeleteIcon(item)">mdi-delete</v-icon>
+      </template>
+    </v-data-table>
+    <v-overlay :value="isLoading">
+      <v-progress-circular indeterminate size="64"/>
+    </v-overlay>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'list',
   data () {
-    return {}
+    return {
+      headers: [
+        { text: 'タイトル', align: 'center', value: 'title' },
+        { text: 'ジャンル', align: 'center', value: 'category' },
+        { text: '購入日', align: 'center', value: 'purchase_date' },
+        { text: '購入者', align: 'center', value: 'buyer' },
+        { text: '編集', align: 'center', value: 'edit-action' },
+        { text: '削除', align: 'center', value: 'delete-action' }
+      ],
+      bookList: [
+        { title: '今夜は眠れない', category: 'ミステリー', purchase_date: 20220810, buyer: '山田太郎', review_content: '面白い' },
+        { title: 'もものかんづめ', category: 'エッセイ', purchase_date: 20220910, buyer: '佐藤次郎', review_content: '普通' },
+        { title: '王さまロボット', category: 'ファンタジー', purchase_date: 20221010, buyer: '鈴木一郎', review_content: '感動した' }
+      ],
+      dialogBookList: {},
+      isShowDialog: false,
+      isShowDeleteDialog: false,
+      searchTitle: '',
+      searchCategory: '',
+      menu: false,
+      textDate: '',
+      pickerDate: '',
+      isLoading: true
+    }
   },
-  created () {},
-  computed: {},
-  methods: {}
+  mounted () {
+    // await google.script.run.withSuccessHandler(function(result) {
+    //  if (result == null){
+    //    app.bookList = {}
+    //  } else {
+    //    app.bookList = result
+    //  }
+    // }).withFailureHandler(function(){
+    //    alert("データを取得出来ません。")
+    // }).selectAllRecord()
+    this.isLoading = false
+  },
+  methods: {
+    onClickEditIcon (bookList) {
+      this.dialogBookList = Object.assign({}, bookList)
+      this.isShowDialog = true
+      this.textDate = this.dialogBookList.purchase_date
+    },
+    onClickDeleteIcon (bookList) {
+      this.dialogBookList = Object.assign({}, bookList)
+      this.isShowDeleteDialog = true
+    },
+    onClickAddBtn () {
+      this.dialogBookList = {}
+      this.isShowDialog = true
+      this.textDate = this.dialogBookList.purchase_date
+    },
+    closeDialog () {
+      this.dialogBookList = {}
+      this.isShowDialog = false
+    },
+    closeDeleteDialog () {
+      this.isShowDeleteDialog = false
+    },
+    onClickInsertUpdateBtn () {
+      // await google.script.run.withSuccessHandler(function () {
+      //  alert('更新しました。')
+      // }).withFailureHandler(function () {
+      //  alert('更新に失敗しました。')
+      // }).insertUpdateRecord(this.dialogBookList)
+      this.closeDialog()
+    },
+    onClickDeleteBtn () {
+      // await google.script.run.withSuccessHandler(function () {
+      //  alert('削除しました。')
+      // }).withFailureHandler(function () {
+      //  alert('削除に失敗しました。')
+      // }).DeleteRecord(this.dialogBookList)
+      this.closeDeleteDialog()
+    },
+    onClickSearchBtn () {
+      // await google.script.run.withSuccessHandler(function () {
+      //  alert('更新しました。')
+      // }).withFailureHandler(function () {
+      //  alert('検索に失敗しました。')
+      // }).selectRecord(searchTitle,searchCategory)
+    },
+    formatDate (date) {
+      const [year, month, day] = date.split('-')
+      this.textDate = `${year}${month}${day}`
+    }
+  }
 }
 </script>
