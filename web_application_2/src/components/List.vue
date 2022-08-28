01e1a268ac676472e130c7fc404ef22ddeed9dc8
nishino <a-nishino@esm.co.jp>
2022-08-28 17:18:23 +0900

    Webアプリ開発2（3-2-4）

--- web_application_2/src/components/List.vue
+++ web_application_2/src/components/List.vue
@@ -1,16 +1,95 @@
 <template>
   <div>
-   一覧表示するよ
+    <v-btn @click="onClickAddBtn">新規登録</v-btn>
+    <v-data-table
+      :headers="headers"
+      :items="bookList"
+      class="elevation-1"
+    >
+      <template v-slot:top>
+        <form-template
+          :dialogBook="dialogBook"
+          :isShowDialog="isShowDialog"
+          :isShowDeleteDialog="isShowDeleteDialog"
+          @closeDialog="closeDialog"
+          @closeDeleteDialog="closeDeleteDialog"
+          @onClickInsertUpdateBtn="onClickInsertUpdateBtn"
+          @onClickDeleteBtn="onClickDeleteBtn"
+        >
+        </form-template>
+      </template>
+        <template v-slot:[`item.edit-action`]="{ item }">
+          <v-icon @click="onClickEditIcon(item)">mdi-pencil</v-icon>
+        </template>
+        <template v-slot:[`item.delete-action`]="{ item }">
+          <v-icon @click="onClickDeleteIcon(item)">mdi-delete</v-icon>
+        </template>
+    </v-data-table>
   </div>
 </template>
 
 <script>
+import Form from './Form'
+
 export default {
-  data () {
-    return {}
+  props: [
+    'bookList'
+  ],
+  components: {
+    'form-template': Form
   },
-  created () {},
-  computed: {},
-  methods: {}
+  data () {
+    return {
+      headers: [
+        { text: 'タイトル', align: 'center', value: 'title' },
+        { text: 'ジャンル', align: 'center', value: 'category' },
+        { text: '購入日', align: 'center', value: 'purchase_date' },
+        { text: '購入者', align: 'center', value: 'buyer' },
+        { text: '編集', align: 'center', value: 'edit-action' },
+        { text: '削除', align: 'center', value: 'delete-action' }
+      ],
+      dialogBook: {},
+      isShowDialog: false,
+      isShowDeleteDialog: false
+    }
+  },
+  methods: {
+    onClickEditIcon (book) {
+      this.dialogBook = Object.assign({}, book)
+      this.isShowDialog = true
+    },
+    onClickDeleteIcon (book) {
+      this.dialogBook = Object.assign({}, book)
+      this.isShowDeleteDialog = true
+    },
+    onClickAddBtn () {
+      this.dialogBook = {}
+      this.isShowDialog = true
+    },
+    closeDialog () {
+      this.dialogBook = {}
+      this.isShowDialog = false
+    },
+    closeDeleteDialog () {
+      this.dialogBook = {}
+      this.isShowDeleteDialog = false
+    },
+    onClickInsertUpdateBtn (dialogBook) {
+      // await google.script.run.withSuccessHandler(function () {
+      //  alert('更新しました。')
+      // }).withFailureHandler(function () {
+      //  alert('更新に失敗しました。')
+      // }).insertUpdateRecord(dialogBook)
+      this.closeDialog()
+    },
+    onClickDeleteBtn (dialogBook) {
+      // await google.script.run.withSuccessHandler(function () {
+      //  alert('削除しました。')
+      // }).withFailureHandler(function () {
+      //  alert('削除に失敗しました。')
+      // }).DeleteRecord(dialogBook)
+      this.closeDeleteDialog()
+    }
+  }
 }
 </script>
--- web_application_2/src/components/Search.vue
+++ web_application_2/src/components/Search.vue
@@ -1,16 +1,28 @@
 <template>
-  <div>
-   検索するよ
-  </div>
+  <v-card-title>
+      <v-text-field v-model="searchTitle" append-icon="mdi-magnify" label="タイトルの文字列検索" />
+      <v-select
+        v-model="searchCategory"
+        :items="bookList"
+        item-text="category"
+        item-value="category"
+        label="ジャンル検索"
+       />
+      <v-btn @click="onClickSearchBtn">検索</v-btn>
+  </v-card-title>
 </template>
 
 <script>
 export default {
-  data () {
-    return {}
-  },
-  created () {},
-  computed: {},
-  methods: {}
+  props: [
+    'bookList',
+    'searchTitle',
+    'searchCategory'
+  ],
+  methods: {
+    onClickSearchBtn () {
+      this.$emit('onClickSearchBtn', this.searchTitle, this.searchCategory)
+    }
+  }
 }
 </script>
