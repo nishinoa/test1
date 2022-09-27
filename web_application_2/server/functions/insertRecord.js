08b778cbb7580dcbe71f89226ea8976dd61fa718
nishino <a-nishino@esm.co.jp>
2022-09-27 11:49:16 +0900

    Webアプリ開発2（3-2-5）方針変更後再修正-指摘事項取込２

--- web_application_2/server/functions/insertRecord.js
+++ web_application_2/server/functions/insertRecord.js
@@ -4,7 +4,7 @@ export default function insertRecord (dialogBook) {
         const sql = `INSERT INTO web_book_nishinoa 
                      (title, category, purchase_date, buyer, review_content)
                      values (?, ?, ?, ?, ?)`;
-        const statement = executeSql(connection,sql);
+        const statement = prepareSql(connection,sql);
         statement.setString(1, dialogBook.title);
         statement.setString(2, dialogBook.category);
         statement.setString(3, dialogBook.purchase_date);
